059388fe5715b2886e7453114ddbfb23f057b792
wib030 <localizedlyss@gmail.com>
2025-08-24 17:27:55 +1000

    Update items.ts

--- data/mods/gen4/items.ts
+++ data/mods/gen4/items.ts
@@ -651,13 +651,13 @@ export const Items: import('../../../sim/dex-items').ModdedItemDataTable = {
 					for (const targetCondition of removeTarget) {
 						if (target.side.removeSideCondition(targetCondition)) {
 							if (!removeAll.includes(targetCondition)) continue;
-							this.add('-sideend', target.side, this.dex.conditions.get(targetCondition).name, '[from] move: Fling', `[of] ${source}`);
+							this.add('-sideend', target.side, this.dex.conditions.get(targetCondition).name, '[from] move: Defog', `[of] ${source}`);
 							success = true;
 						}
 					}
 					for (const sideCondition of removeAll) {
 						if (source.side.removeSideCondition(sideCondition)) {
-							this.add('-sideend', source.side, this.dex.conditions.get(sideCondition).name, '[from] move: Fling', `[of] ${source}`);
+							this.add('-sideend', source.side, this.dex.conditions.get(sideCondition).name, '[from] move: Defog', `[of] ${source}`);
 							success = true;
 						}
 					}
