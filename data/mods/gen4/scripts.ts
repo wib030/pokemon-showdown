2301cb13311a17694dd9b0cac4aa984a357d5e13
wib030 <localizedlyss@gmail.com>
2025-08-28 12:26:22 +1000

    Reapply "update these"
    This reverts commit 13d8d512bf634f574ee3496de5fdc0aaa959a3d5.

--- data/mods/gen4/scripts.ts
+++ data/mods/gen4/scripts.ts
@@ -142,9 +142,6 @@ export const Scripts: ModdedBattleScriptsData = {
 				if (pokemon.hasAbility('normalize') && move.id !== 'judgment') {
 					moveType = 'Normal';
 				}
-				if (move.id === 'naturalgift' && item.naturalGift) {
-					moveType = item.naturalGift.type;
-				}
 				if (move.id === 'hiddenpower') {
 					moveType = pokemon.hpType || 'Dark';
 				}
