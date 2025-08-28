f9a7dc2c65fab4239ec72b527b586840ce5bbeed
wib030 <localizedlyss@gmail.com>
2025-08-28 12:07:50 +1000

    Revert "check this?"
    This reverts commit 780c8e9f9e6151ec8c3b48bdd2d13c3b96e8aa8c.

--- data/mods/gen4/scripts.ts
+++ data/mods/gen4/scripts.ts
@@ -135,6 +135,8 @@ export const Scripts: ModdedBattleScriptsData = {
 				let item = pokemon.getItem();
 				if (move.id === 'judgment') {
 					moveType = pokemon.species.types[0];
+				} else if (move.id === 'fling' && item.onPlate) {
+					moveType = item.onPlate;
 				} else if (pokemon.hasAbility('normalize')) {
 					moveType = 'Normal';
 				}
