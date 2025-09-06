b83433e45f2624a66502597ac3e4fe1c85593bdb
wib030 <localizedlyss@gmail.com>
2025-09-06 18:18:15 +1000

    define these properly

--- data/random-battles/gen4/teams.ts
+++ data/random-battles/gen4/teams.ts
@@ -677,6 +677,8 @@ export class RandomGen4Teams extends RandomGen5Teams {
 		const movePool: string[] = Array.from(set.movepool);
 		const preferredTypes = set.preferredTypes;
 		const preferredType = this.sampleIfArray(preferredTypes) || '';
+		const evPool: number[] = Array.from(set.evs);
+		const ivPool: number[] = Array.from(set.ivs);
 
 		let ability = '';
 		let item = undefined;
