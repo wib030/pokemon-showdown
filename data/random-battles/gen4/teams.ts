f66fee1ccf9703788070e3dd9ca9f3a646834471
wib030 <localizedlyss@gmail.com>
2025-09-06 17:51:59 +1000

    ummmm

--- data/random-battles/gen4/teams.ts
+++ data/random-battles/gen4/teams.ts
@@ -677,8 +677,8 @@ export class RandomGen4Teams extends RandomGen5Teams {
 		const movePool: string[] = Array.from(set.movepool);
 		const preferredTypes = set.preferredTypes;
 		const preferredType = this.sampleIfArray(preferredTypes) || '';
-		const ivPool[5] = set.ivs;
-		const evPool[5] = set.evs;
+		const ivPool[] = set.ivs;
+		const evPool[] = set.evs;
 
 		let ability = '';
 		let item = undefined;
