88d122b201518d92ed6ad0dbf49fe10f56db9b2b
wib030 <localizedlyss@gmail.com>
2025-09-09 13:24:00 +1000

    ???

--- data/mods/gen4/moves.ts
+++ data/mods/gen4/moves.ts
@@ -2233,12 +2233,19 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 				return null;
 			}
 		},
-		onAnyModifyBoost(boosts, pokemon, source) {
-			if (source === pokemon) return;
-			if (source === this.activePokemon && pokemon === this.activeTarget) {
-				if (boosts['def'] > 0) {
-					boosts['def'] = 0;
-				}
+		onAnyModifyBoost(boosts, pokemon) {
+			const unawareUser = this.effectState.target;
+			if (unawareUser === pokemon) return;
+			if (unawareUser === this.activePokemon && pokemon === this.activeTarget) {
+				boosts['def'] = 0;
+				boosts['spd'] = 0;
+				boosts['evasion'] = 0;
+			}
+			if (pokemon === this.activePokemon && unawareUser === this.activeTarget) {
+				boosts['atk'] = 0;
+				boosts['def'] = 0;
+				boosts['spa'] = 0;
+				boosts['accuracy'] = 0;
 			}
 		},
 	},
