9b418a2cb4965025072b622bf14cc2a2c4f25955
wib030 <localizedlyss@gmail.com>
2025-09-09 13:30:02 +1000

    tries this for focus punch??

--- data/mods/gen4/moves.ts
+++ data/mods/gen4/moves.ts
@@ -2234,18 +2234,10 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 			}
 		},
 		onAnyModifyBoost(boosts, pokemon) {
-			const unawareUser = this.effectState.target;
-			if (unawareUser === pokemon) return;
-			if (unawareUser === this.activePokemon && pokemon === this.activeTarget) {
-				boosts['def'] = 0;
-				boosts['spd'] = 0;
-				boosts['evasion'] = 0;
-			}
-			if (pokemon === this.activePokemon && unawareUser === this.activeTarget) {
-				boosts['atk'] = 0;
-				boosts['def'] = 0;
-				boosts['spa'] = 0;
-				boosts['accuracy'] = 0;
+			if (this.activeMove === 'focuspunch' && pokemon === this.activeTarget) {
+				if (boosts['def'] > 0) {
+					boosts['def'] = 0;
+				}
 			}
 		},
 	},
