ac8e4919697eee8473f69cda66836625d1e6850f
wib030 <localizedlyss@gmail.com>
2025-09-09 11:03:59 +1000

    remove contact from peekaboo

--- data/moves.ts
+++ data/moves.ts
@@ -22202,7 +22202,7 @@ export const Moves: import('../sim/dex-moves').MoveDataTable = {
 		name: "Peek-A-Boo",
 		pp: 10,
 		priority: 0,
-		flags: { contact: 1, protect: 1, mirror: 1, metronome: 1 },
+		flags: { protect: 1, mirror: 1, metronome: 1 },
 		onHit(target) {
 			if (target.status === 'slp') target.cureStatus();
 		},
