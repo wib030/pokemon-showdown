94f2445319fbe15523e8546e55603274ee6501a1
wib030 <localizedlyss@gmail.com>
2025-09-16 11:55:49 +1000

    allow anyone to hold the griseous orb

--- sim/team-validator.ts
+++ sim/team-validator.ts
@@ -702,9 +702,6 @@ export class TeamValidator {
 		problem = this.checkItem(set, item, setHas);
 		if (problem) problems.push(problem);
 		if (ruleTable.has('obtainablemisc')) {
-			if (dex.gen === 4 && item.id === 'griseousorb' && species.num !== 487) {
-				problems.push(`${set.name} cannot hold the Griseous Orb.`, `(In Gen 4, only Giratina could hold the Griseous Orb).`);
-			}
 			if (dex.gen <= 1 || dex.currentMod === 'gen7letsgo') {
 				if (item.id) {
 					// no items allowed
