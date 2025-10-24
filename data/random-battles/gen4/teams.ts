d60de23087228d19337a72de1e6c5b2c843d7333
wib030 <localizedlyss@gmail.com>
2025-10-24 21:52:44 +1100

    Update teams.ts

--- data/random-battles/gen4/teams.ts
+++ data/random-battles/gen4/teams.ts
@@ -716,7 +716,7 @@ export class RandomGen4Teams extends RandomGen5Teams {
 			if (leadNum > 1 && LEAD_ROLES.includes(set.role)) continue;
 			
 			// Enforce Removal if the team does not have removal
-			if (ensureRemoval && && hasRemovalSet && !REMOVAL_ROLES.includes(set.role)) continue;
+			if (ensureRemoval && hasRemovalSet && !REMOVAL_ROLES.includes(set.role)) continue;
 			
 			// Prevent Removal if the team already has more than one removal
 			if (removalNum > 1 && REMOVAL_ROLES.includes(set.role)) continue;
