b34c64db8df4eba469d8a6c0f91e7633135849dd
wib030 <localizedlyss@gmail.com>
2025-10-31 20:36:43 +1100

    add swampert, ttar, torkoal,  and uxie sets

--- sim/global-types.ts
+++ sim/global-types.ts
@@ -570,7 +570,8 @@ declare namespace RandomTeamsTypes {
 		'Hail Attacker' | 'Sand Attacker'| 'Fast Lead' | 'Bulky Lead' | 'Choice Trick' | 'Fast Screens Setter' | 
 		'Bulky Screens Setter' | 'Glass Cannon' | 'Fling Setup' | 'TR Setter' | 'TR Attacker' | 'Baton Passer' |
 		'Fast Pivot' | 'Bulky Pivot' | 'Defog' | 'TR Lead' | 'Sun Setup' | 'Anti-Lead' | 'Switch Trapper' | 
-		'Sand Lead' | 'Sun Lead' | 'Hail Lead' | 'Rain Lead' | 'Hail Support';
+		'Sand Lead' | 'Sun Lead' | 'Hail Lead' | 'Rain Lead' | 'Hail Support' | 'Sun Support' | 'Offensive Tank' |
+		'Status Tank' | 'AV Tank';
 		
 	export type Inclination = 'Mixed' | 'Physical' | 'Special' | 'Support' | 'Unset';
 }
