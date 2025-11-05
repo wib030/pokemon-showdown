8fd7a500919d7f6cd004b8d7ec3736f6b26ba0d3
wib030 <localizedlyss@gmail.com>
2025-11-06 01:19:34 +1100

    moves this here

--- data/abilities.ts
+++ data/abilities.ts
@@ -6210,7 +6210,21 @@ export const Abilities: import('../sim/dex-abilities').AbilityDataTable = {
 			}
 			return [type1, type2];
 		},
-		flags: {},
+		onBeforeSwitchIn(pokemon) {
+			pokemon.setType(pokemon.baseSpecies.types); // This will cause Antitype to trigger on switch-in as intended
+		},
+		onSwitchIn(pokemon) {
+			if (pokemon.ability === 'antitype') {
+				const item = pokemon.getItem();
+				const targetForme = (item?.onPlate ? 'Giratina-' + item.onPlate : 'Giratina');
+				if (targetForme !== 'Giratina') {
+					if (pokemon.species.name !== targetForme) {
+						pokemon.formeChange(targetForme);
+					}
+				}
+			}
+		},
+		flags: { failroleplay: 1, noreceiver: 1, noentrain: 1, notrace: 1, failskillswap: 1, cantsuppress: 1 },
 		name: "Antitype",
 		rating: 4,
 		num: -29,
