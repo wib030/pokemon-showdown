e8b69678a81e19f3e1b806ea379a16a9104d34ed
wib030 <localizedlyss@gmail.com>
2025-11-12 15:48:42 +1100

    put this back in

--- data/random-battles/gen4/teams.ts
+++ data/random-battles/gen4/teams.ts
@@ -1071,7 +1071,7 @@ export class RandomGen4Teams extends RandomGen5Teams {
 
 		// let pokemonPool = this.getPokemonPool(type, pokemon, isMonotype, pokemonList);
 
-		while (pokemon.length < this.maxTeamSize) {
+		while (baseSpeciesPool.length && pokemon.length < this.maxTeamSize) {
 			[pokemonPool, baseSpeciesPool] = this.getPokemonPool(type, pokemon, isMonotype, pokemonList);
 			
 			const baseSpecies = this.sample(baseSpeciesPool);
