6734a5b1eecba3cc272e2e947e4a0ee91c24f209
wib030 <localizedlyss@gmail.com>
2025-12-10 01:10:55 +1100

    Revert "make this a variable"
    This reverts commit b9ad25bc184ad06b712f28c3e75624afaa86f5e9.

--- sim/pokemon.ts
+++ sim/pokemon.ts
@@ -623,9 +623,8 @@ export class Pokemon {
 		if (!unmodified) {
 			const statTable: { [s in StatIDExceptHP]: string } = { atk: 'Atk', def: 'Def', spa: 'SpA', spd: 'SpD', spe: 'Spe' };
 			stat = this.battle.runEvent('Modify' + statTable[statName], this, null, null, stat);
-			const cowardBoosted = this.battle.activeMove.cowardBoosted | false;
-			if (statName === 'spe' && cowardBoosted === true) {
-				stat *= 2;
+			if (statName === 'spe' && this.battle.activeMove.cowardBoosted) {
+				if (this.battle.activeMove.cowardBoosted === true) stat *= 2;
 			}
 		}
 
