93ef05df6c15885765758e059ad5bb2a49f0c7c3
wib030 <localizedlyss@gmail.com>
2026-06-19 14:38:01 +1000

    Update conditions.ts

--- data/mods/gen4/conditions.ts
+++ data/mods/gen4/conditions.ts
@@ -54,16 +54,6 @@ export const Conditions: import('../../../sim/dex-conditions').ModdedConditionDa
 			if (sourceEffect.effectType === 'Move') {
 				accuracy = move.accuracy;
 				
-				if ((move.forceSTAB || source.hasType(moveType)) && accuracy != true) {
-					accuracy = accuracy * 1.1;
-				}
-
-				if (move.ohko) {
-					accuracy = 30 + source.level - target.level;
-				}
-
-				accuracy = this.battle.runEvent('ModifyAccuracy', target, source, move, accuracy);
-				
 				if (accuracy === true) {
 					accuracy = 100;
 				}
