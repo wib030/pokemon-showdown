d8f364489b10b97387f793f81ec1f3239890cc6e
wib030 <localizedlyss@gmail.com>
2026-07-11 22:11:48 +1000

    pretty sure this function already considers boosts

--- data/mods/gen4/moves.ts
+++ data/mods/gen4/moves.ts
@@ -1857,6 +1857,7 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 				this.event.modifier = 1;
 				let attacker = move.allies!.shift()!;
 				let attackStat = attacker.getStat('atk', false, true);
+				/*
 				const boostTable = [1, 1.5, 2, 2.5, 3, 3.5, 4];
 				let boost = pokemon.boosts['atk'];
 				if (boost > 6) boost = 6;
@@ -1866,6 +1867,7 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 				} else {
 					attackStat = Math.floor(attackStat / boostTable[-boost]);
 				}
+				*/
 				
 				if (attacker.hasType(move.type) && attacker.ability === 'adaptability') {
 					attackStat *= 2;
